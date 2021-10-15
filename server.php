b38ae4b35dc0587be959e0aaca2484b721b937ca
WalterFrongia <walter.frongia@libero.it>
2021-10-15 16:39:23 +0200

    Incluso in server il file database e impostato header e proprietà echo per richiamare array

---
+++ server.php
@@ -0,0 +1,11 @@
+<!-- includere il database -->
+
+<?php
+    require __DIR__ . '/db/database.php';
+
+    header('Content-Type: application/jason');
+    
+    //inserire nome array del database 
+    echo json_encode()
+
+?>
